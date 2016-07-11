bea91f716eabb845f68db0c5e18f1d2378f15d64
yvkschaefer <yvkschaefer@gmail.com>
2016-07-11 17:40:49 +0000

    a wild interval

--- hello-world.js
+++ hello-world.js
@@ -2,12 +2,23 @@
 // Add an instruction to your program that will output "Hello World Again!!" 10 seconds after the 
 //program was run. Save, commit and push.
 
-function hello(){
-    console.log('Hello World!');
-    setTimeout(function(){
-        console.log('Hello World Again!!')
-    }, 10000)
-}
+// function hello(){
+//     console.log('Hello World!');
+//     setTimeout(function(){
+//         console.log('Hello World Again!!')
+//     }, 10000)
+// }
 
-hello();
+// hello();
+
+
+//Q2. Write a piece of code using setTimeout that prints "Hello World!" every 10 seconds forever.
+
+function repeat(){
+     setTimeout(function() {
+    console.log('Hello World!');
+      return repeat();
+  }, 10000)
+}
+repeat();
 
