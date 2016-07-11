72587238f8aaed80d90fdf16005d5d411d851bf3
yvkschaefer <yvkschaefer@gmail.com>
2016-07-11 17:28:31 +0000

    first program

---
+++ hello-world.js
@@ -0,0 +1,13 @@
+//Q1. Write a simple node program that outputs "Hello World!" to the console.
+// Add an instruction to your program that will output "Hello World Again!!" 10 seconds after the 
+//program was run. Save, commit and push.
+
+function hello(){
+    console.log('Hello World!');
+    setTimeout(function(){
+        console.log('Hello World Again!!')
+    }, 10000)
+}
+
+hello();
+
